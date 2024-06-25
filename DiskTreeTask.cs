036461079963eb21be3974059d5a23240d4c1e19
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-06-25 18:38:18 +0300

    Add DiskTreeClass functionality; Upd README

--- DiskTreeTask.cs
+++ DiskTreeTask.cs
@@ -1,1 +1,63 @@
-﻿namespace DiskTree;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace DiskTree;
+public static class DiskTreeTask
+{
+    public static List<string> Solve (List<string> input)
+    {
+        Node directoriesTree = BuildDirectoriesTree(input);
+        List<string> result = new ();
+        CreateFormattedDirectoriesList(directoriesTree, result);
+        return result;
+    }
+
+    class Node
+    {
+        public string DirName { get; }
+        public List<Node> Subdirs { get; }
+
+        public Node(string name)
+        {
+            DirName = name;
+            Subdirs = new List<Node>();
+        }
+    }
+
+    static Node BuildDirectoriesTree(List<string> directoriesPaths)
+    {
+        var root = new Node("");
+        foreach (var directoryPath in directoriesPaths)
+        {
+            var splitedPath = directoryPath.Split('\\');
+            var currentNode = root;
+            foreach (var directory in splitedPath)
+            {
+                var existingSubdirs = currentNode.Subdirs.Find(c => c.DirName == directory);
+                if (existingSubdirs == null)
+                {
+                    var newSubdir = new Node(directory);
+                    currentNode.Subdirs.Add(newSubdir);
+                    currentNode = newSubdir;
+                }
+                else
+                {
+                    currentNode = existingSubdirs;
+                }
+            }
+        }
+
+        return root;
+    }
+
+    static void CreateFormattedDirectoriesList(Node node, List<string> directories, int depth = 0)
+    {
+        foreach (var subdir in node.Subdirs.OrderBy(c => c.DirName, StringComparer.Ordinal))
+        {
+            directories.Add(new string(' ', depth) + subdir.DirName);
+            CreateFormattedDirectoriesList(subdir, directories, depth + 1);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # DiskTree
+
+This project solves directories tree task: it creates a formatted directories list from known directories paths.
+
+Input: List<string>, where each string represents one of the known paths.
+Each string is no longer than 80 symbols, doesn't have whitespaces,
+doesn't repeat in List and consists of directories, split by '\'.
+
+Output: a formatted directories tree, represented by List<string>.
+Each name of a directory consists of whitespaces (as many as a directory is deep) and its name.
+Subdirectories follows main directory in lexicographical order.
+Top-level directories don’t have whitespaces in their names and are sorted in lexicographical order.
